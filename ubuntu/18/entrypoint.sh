a631a208a578a070bd953a1b61372679e3bb0717
Luca Milanesio <luca.milanesio@gmail.com>
2019-05-09 16:51:30 +0100

    Run init during docker build
    The Gerrit init step is responsible for extracting the gerrit startup
    script, the service wrapper and the installation of plugins.
    Running it only at runtime would not allow existing sites to work
    properly or even to be migrated.
    
    Introduce a 'init' parameter to the entrypoint.sh so that it can be
    invoked at build time and from the docker-compose.yaml for upgrading
    existing sites.
    
    Move also the java.security.egd=file:/dev/./urandom setting to
    entrypoint.sh, so to make sure that gets executed also on externally
    mounted configuration files.
    
    Change-Id: I903b2ed49278f5c929d63f3e65ac1bb96b1de081

--- ubuntu/18/entrypoint.sh
+++ ubuntu/18/entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -e
 
-if [ ! -d /var/gerrit/git/All-Projects.git ]
+if [ ! -d /var/gerrit/git/All-Projects.git ] || [ "$1" == "init" ]
 then
   echo "Initializing Gerrit site ..."
   java -jar /var/gerrit/bin/gerrit.war init --batch --install-all-plugins -d /var/gerrit
   java -jar /var/gerrit/bin/gerrit.war reindex -d /var/gerrit
+  git config -f /var/gerrit/etc/gerrit.config --add container.javaOptions "-Djava.security.egd=file:/dev/./urandom"
 fi
 
 git config -f /var/gerrit/etc/gerrit.config gerrit.canonicalWebUrl "${CANONICAL_WEB_URL:-http://$HOSTNAME}"
 
-echo "Running Gerrit ..."
-exec /var/gerrit/bin/gerrit.sh run
+if [ "$1" != "init" ]
+then
+  echo "Running Gerrit ..."
+  exec /var/gerrit/bin/gerrit.sh run
+fi
