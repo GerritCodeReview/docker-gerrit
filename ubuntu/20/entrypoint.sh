727e9817cfce887b0d0d4ddce729130611b06218
Luca Milanesio <luca.milanesio@gmail.com>
2020-05-01 16:59:34 +0100

    Add JAVA_OPTS to suppress Java 11-related warnings during init
    Bug: Issue 12639
    Change-Id: Iae3289d7703a5c8a3d7cc9e257cd0c1403bf9570

--- ubuntu/20/entrypoint.sh
+++ ubuntu/20/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 
+export JAVA_OPTS='--add-opens java.base/java.net=ALL-UNNAMED --add-opens java.base/java.lang.invoke=ALL-UNNAMED'
+
 if [ ! -d /var/gerrit/git/All-Projects.git ] || [ "$1" == "init" ]
 then
   echo "Initializing Gerrit site ..."
-  java -jar /var/gerrit/bin/gerrit.war init --batch --install-all-plugins -d /var/gerrit
-  java -jar /var/gerrit/bin/gerrit.war reindex -d /var/gerrit
+  java $JAVA_OPTS -jar /var/gerrit/bin/gerrit.war init --batch --install-all-plugins -d /var/gerrit
+  java $JAVA_OPTS -jar /var/gerrit/bin/gerrit.war reindex -d /var/gerrit
   git config -f /var/gerrit/etc/gerrit.config --add container.javaOptions "-Djava.security.egd=file:/dev/./urandom"
   git config -f /var/gerrit/etc/gerrit.config --add container.javaOptions "--add-opens java.base/java.net=ALL-UNNAMED"
   git config -f /var/gerrit/etc/gerrit.config --add container.javaOptions "--add-opens java.base/java.lang.invoke=ALL-UNNAMED"
