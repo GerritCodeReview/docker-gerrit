4a825351311523f82c606738b2ec79a97964ad90
Jacek Centkowski <geminica.programs@gmail.com>
2022-11-19 12:51:26 +0100

    Add helper script to perform multi-platform images builds
    The `build_multiplatform.sh` file was introduced to automate the
    multi-platform images build process.
    One calls
    
      ./build_multiplatform.sh
    
    to build both ubuntu and almalinux images for the current system
    platform (either `amd64` or `arm64`) and load it to the local
    docker registry.
    
    One calls:
    
      ./build_multiplatform.sh --push
    
    in order to build (for both platforms) and push to the dockerhub's
    gerritcodereview account.
    
    Notes:
    * script takes care of multi-platform builder creation and clean-up
    * almalinux is additionlly tagged as the default release image
    * Gerrit installation packages need to be build and published using the
      changes made in Iedcaf6511 as otherwise image build will fail for
      arm64 platform
    
    Change-Id: I5baaed04ec8a82652c46d1f4ef151928690d7cfb
    Depends-On: Iedcaf6511fb091775c1f2d0fb1596531efbba7e1

---
+++ build_multiplatform.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# By default it builds single platform images (alamalinux and ubuntu) and loads them into
+# local docker (--load is assumed) however one can specify --push and it will build for
+# both 'amd64' and 'arm64' architectures and push to the dockerhub.
+# Note that in the later case one needs to be loged in to 'gerritcodereview' account.
+
+DESTINATION=$1
+
+if [ -z "$DESTINATION" ]; then
+  DESTINATION="--load"
+fi
+
+if [[ $DESTINATION == *load ]]; then
+  if [[ $(uname -m) == *arm64* ]]; then
+    PLATFORMS="linux/arm64"
+  else
+    PLATFORMS="linux/amd64"
+  fi
+else
+  PLATFORMS="linux/amd64,linux/arm64"
+fi
+
+echo "### Building images for $DESTINATION destination and $PLATFORMS"
+
+BUILDER=gerrit-multiplatform-image-builder
+DOCKER_USER=gerritcodereview/gerrit
+
+function create_builder() {
+  if [[ "$OSTYPE" == "linux"* ]]; then
+    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  fi
+  docker buildx create --name $BUILDER --platform "$PLATFORMS" --driver docker-container --use
+  docker buildx inspect --bootstrap
+}
+
+STATUS="$(docker buildx inspect $BUILDER 2>&1 | grep Status:)"
+if [ "$?" -ne "0" ]; then
+  set -e
+  echo "### Multi-platform builder $BUILDER doesn't exist and will be created."
+  create_builder
+else
+  set -e
+  STATUS="${STATUS##* }"
+  if [[ $STATUS == *running* ]]; then
+    echo "### Multi-platform builder $BUILDER is up and running."
+  else
+    echo "### Multi-platform builder $BUILDER exists but it doesn't run. It will be re-created."
+    docker buildx rm $BUILDER
+    create_builder
+  fi
+fi
+
+echo
+echo "### Building almalinux multi-platform: [$PLATFORMS] iamges"
+$(cd almalinux/8 && docker buildx build --platform "$PLATFORMS" --no-cache -t $DOCKER_USER:$(git describe)-almalinux8 -t $DOCKER_USER:$(git describe) $DESTINATION .)
+
+echo
+echo "### Building ubuntu multi-platform: [$PLATFORMS] iamges"
+$(cd ubuntu/20 && docker buildx build --platform "$PLATFORMS" --no-cache -t $DOCKER_USER:$(git describe)-ubuntu20 $DESTINATION .)
+
+echo
+echo "### Removing multi-platform builder"
+echo y | docker buildx prune
+docker buildx stop $BUILDER
+docker buildx rm $BUILDER
